09303077b983f7b12b13bb9e78b3c22c678299cd
alongre <alongre@gmail.com>
2017-07-26 05:28:38 +0300

    adding treeView

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import { ClockContainer } from './components/clock/Clock-container';
+import Navbar from './components/navbar/navbar';
 import { TreeViewContainer } from './components/treeView/treeView-Container';
 import './App.css';
 
 const App = () => {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-        </div>
+        <Navbar className="App-header"/>
         <TreeViewContainer/>
       </div>
     );
---
+++ src/components/tableView/tableView.js
@@ -0,0 +1,22 @@
+// import { Table,Column } from '@blueprintjs/table';
+
+// import React, { Component, PropTypes } from 'react'
+
+// class TableView extends Component {
+//     render () {
+//         return (
+//             <Table numRows={5}>
+//                 <Column />
+//                 <Column />
+//                 <Column />
+//             </Table>
+//         )
+//     }
+// }
+
+// componentName.propTypes = {
+
+// }
+
+// export default componentName
+
